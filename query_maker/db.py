489e38c24c9bb1b4ee08c2145dfd12b1374801ff
Luci Furtun <lucianfurtun@gmail.com>
2020-12-20 17:37:26 +0200

    Add query maker

---
+++ query_maker/db.py
@@ -0,0 +1,149 @@
+import abc
+from dataclasses import dataclass
+from datetime import date
+from enum import Enum
+from typing import Any
+
+
+class NotAllowedOperatorError(Exception):
+    pass
+
+
+class Operators(Enum):
+    EQ = '='
+    GT = '>'
+    LT = '<'
+    IN = 'IN'
+    NOT_IN = 'NOT IN'
+
+    @classmethod
+    def get_all(cls) -> list:
+        return [item.value for item in cls]
+
+
+LIST_OPERATORS = (Operators.IN.value, Operators.NOT_IN.value)
+
+
+@dataclass
+class Filter:
+    column: 'Column'
+    value: Any
+    operator: str
+
+    def get_value(self) -> str:
+        value = self.value
+
+        return f'{self.column.name} {self.operator} {value}'
+
+
+class ListFilter(Filter):
+    def get_value(self) -> str:
+        value = ', '.join([str(v) for v in self.value])
+        value = f'({value})'
+
+        return f'{self.column.name} {self.operator} {value}'
+
+
+class Column(abc.ABC):
+    name: str
+
+    def __init__(self, name: str, allowed_operators: list = None) -> None:
+        super().__init__()
+        self.name = name
+
+        if allowed_operators is None:
+            self.allowed_operators = Operators.get_all()
+        else:
+            self.allowed_operators = allowed_operators
+
+    def to_sql(self, value):
+        if isinstance(value, list):
+            return [self.py_value_to_sql_value(item) for item in value]
+
+        return self.py_value_to_sql_value(value)
+
+    @abc.abstractmethod
+    def py_value_to_sql_value(self, value):
+        pass
+
+    def eq(self, value) -> Filter:
+        return self._get_filter(value=value, operator=Operators.EQ.value)
+
+    def gt(self, value) -> Filter:
+        return self._get_filter(value=value, operator=Operators.GT.value)
+
+    def lt(self, value) -> Filter:
+        return self._get_filter(value=value, operator=Operators.LT.value)
+
+    def in_(self, value) -> Filter:
+        return self._get_filter(value=value, operator=Operators.IN.value)
+
+    def not_in(self, value) -> Filter:
+        return self._get_filter(value=value, operator=Operators.NOT_IN.value)
+
+    def __eq__(self, value):
+        return self.eq(value)
+
+    def __gt__(self, value):
+        return self.gt(value)
+
+    def __lt__(self, value):
+        return self.lt(value)
+
+    def _get_filter(self, value, operator) -> Filter:
+        if operator not in self.allowed_operators:
+            raise NotAllowedOperatorError()
+
+        if operator in LIST_OPERATORS:
+            return ListFilter(
+                column=self,
+                value=self.to_sql(value),
+                operator=operator
+            )
+
+        return Filter(
+            column=self,
+            value=self.to_sql(value),
+            operator=operator
+        )
+
+
+class IntegerColumn(Column):
+    def py_value_to_sql_value(self, value: int):
+        return int(value)
+
+
+class StringColumn(Column):
+    def py_value_to_sql_value(self, value: str):
+        return f'"{value}"'
+
+
+class DateColumn(Column):
+    def py_value_to_sql_value(self, value: date):
+        return f'"{value.isoformat()}"'
+
+
+class Table:
+    @classmethod
+    def query(cls, *filters) -> str:
+        final_query = cls.generate_query(*filters)
+        final_query += ';'
+
+        return final_query
+
+    @classmethod
+    def generate_query(cls, *filters) -> str:
+        table_name = cls.Meta.table_name if hasattr(cls, 'Meta') else cls.__name__
+
+        # Assume that we always select *.
+        query = f'SELECT * FROM "{table_name}"'
+
+        if not filters:
+            return query
+
+        # Assume that we only use AND operator.
+        where_expression = ' AND '.join([f.get_value() for f in filters])
+
+        query += ' WHERE ' + where_expression
+
+        return query
---
+++ query_maker/test_generate_query.py
@@ -0,0 +1,44 @@
+from datetime import date
+
+import pytest
+
+from query_maker import db
+from query_maker.db import Operators
+
+
+class SomeTable(db.Table):
+    id = db.IntegerColumn(name='id')
+    url = db.StringColumn(name='url', allowed_operators=[Operators.EQ.value])
+    date = db.DateColumn(name='date')
+    rating = db.IntegerColumn(name='rating')
+
+    class Meta:
+        table_name = 'some_table'
+
+
+def test_generate_sql_query():
+    query = SomeTable.query(SomeTable.id == 3)
+    assert query == 'SELECT * FROM "some_table" WHERE id = 3;'
+
+    query = SomeTable.query(SomeTable.id == 3, SomeTable.id == 5)
+    assert query == 'SELECT * FROM "some_table" WHERE id = 3 AND id = 5;'
+
+    query = SomeTable.query(SomeTable.id.in_([3]))
+    assert query == 'SELECT * FROM "some_table" WHERE id IN (3);'
+
+    query = SomeTable.query(SomeTable.id.not_in([3, 5]))
+    assert query == 'SELECT * FROM "some_table" WHERE id NOT IN (3, 5);'
+
+    query = SomeTable.query(SomeTable.id > 3, SomeTable.url == 'abc@abc.com')
+    assert query == 'SELECT * FROM "some_table" WHERE id > 3 AND url = "abc@abc.com";'
+
+    query = SomeTable.query(SomeTable.id < 3, SomeTable.id > 0)
+    assert query == 'SELECT * FROM "some_table" WHERE id < 3 AND id > 0;'
+
+    query = SomeTable.query(SomeTable.date > date(2016, 1, 1))
+    assert query == 'SELECT * FROM "some_table" WHERE date > "2016-01-01";'
+
+
+def test_generate_sql_query_limited_operators():
+    with pytest.raises(db.NotAllowedOperatorError):
+        query = SomeTable.query(SomeTable.url > 'test@test.com')
